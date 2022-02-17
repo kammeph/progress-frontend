b36391c911d9acacd92a0efe2b06f444d9ee1d8a
kammeph <ph.kammerer@googlemail.com>
2022-02-17 21:37:20 +0100

    feat(environment): Adjust environment for deployment on home server

--- progress/src/assets/env.js
+++ progress/src/assets/env.js
@@ -1,6 +1,6 @@
-(function(window) {
-    window["env"] = window["env"] || {};
-  
-    // Environment variables
-    window["env"]["apiEndpoint"] = "http://localhost:8000/api";
-  })(this);
+(function (window) {
+  window['env'] = window['env'] || {};
+
+  // Environment variables
+  window['env']['apiEndpoint'] = 'http://localhost:3000/api';
+})(this);
